d389bbad08eae5722348cbc46894bf38f6c80c4f
DustyC <123617229+DustyCs@users.noreply.github.com>
2024-12-12 17:13:05 +0800

    Changed player image to latest

--- classes/player.py
+++ classes/player.py
@@ -1,20 +1,4 @@
-"""
-    Player = Bob
-
-    Bob needs control.
-    Bob needs to be able to smile.
-    ...
-
-    TODO
-
-    Need to load appropriate image when player is moving. Currently Bob has one used for all directions
-    Add slime like animation for Bob
-
-    Stop Bob from going outside the computer screen!
-    Bob is leaving a trail behind 0 o 0 - clean it! - Animations are images - Bob is an image that keeps getting drawn
-    Bob is sad about being too big :( - Rescale him
-
-"""
+import pygame # type: ignore
 
 class Player:
     player_image = None
@@ -23,6 +7,10 @@ class Player:
     player_speed = 5
     movement_state = "idle"
     animation_frame = 0
+
+    image = None
+    frame_x = 0
+    frame_y = 1
 
     side_imgs = []
     up_imgs = []
@@ -60,17 +48,39 @@ class Player:
             if self.animation_frame >= 5:
                 self.animation_frame = 0
 
+    def getImage(self, sheet, frame_x, frame_y, width, height, scale, color):
+        image = pygame.Surface((width, height)).convert_alpha()
+        image_base = 64
+        image.blit(sheet, (0, 0), ((width * frame_x), (image_base * frame_y), width, height))
+        image = pygame.transform.scale(image, (width * scale, height * scale))
+        image.set_colorkey(color)
+        return image
+
     def render(self, window):
-        window.fill((0, 0, 0))
-        window.blit(self.player_image, self.player_rect)
+        # window.fill((0, 0, 0))
+        # window.blit(self.player_sprite_idle, (0, 0))
+        window.blit(self.getImage(self.player_sprite_idle, self.frame_x, self.frame_y, 64, 64, 3, (0,0,0)), self.player_rect)
+
+
+
+
+
+        # window.blit(self.player_image, self.player_rect)
+
+
+
+
+
 
         # need cleaning and fixing - DRY - also reset the animation frame
-        if self.movement_state == "side":
-            window.fill((0, 0, 0))                
-            window.blit(self.side_imgs[self.animation_frame], self.player_rect)
-        if self.movement_state == "up":
-            window.fill((0, 0, 0))                
-            window.blit(self.up_imgs[self.animation_frame], self.player_rect)
-        if self.movement_state == "down":
-            window.fill((0, 0, 0))                
-            window.blit(self.down_imgs[self.animation_frame], self.player_rect)
+        # scrapped
+        # could be useful though
+        # if self.movement_state == "side":
+        #     window.fill((0, 0, 0))                
+        #     window.blit(self.side_imgs[self.animation_frame], self.player_rect)
+        # if self.movement_state == "up":
+        #     window.fill((0, 0, 0))                
+        #     window.blit(self.up_imgs[self.animation_frame], self.player_rect)
+        # if self.movement_state == "down":
+        #     window.fill((0, 0, 0))                
+        #     window.blit(self.down_imgs[self.animation_frame], self.player_rect)
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ pygame.init()
 
 window = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
-FPS = 20
+FPS = 60
 run = True
 
 # Player
@@ -16,21 +16,22 @@ player.player_sprite_idle = pygame.image.load('design/Slime/Idle/Slime1_Idle_ful
 
 player.player_image = pygame.image.load('design/Bob.png').convert_alpha()
 player.player_rect = player.player_image.get_rect()
+
+
 player.side_imgs = [pygame.image.load('design/sides/side' + str(x) + '.png') for x in range(4)] # list comprehension
 player.up_imgs = [pygame.image.load('design/up/up' + str(x) + '.png') for x in range(6)]
 player.down_imgs = [pygame.image.load('design/down/down' + str(x) + '.png') for x in range(6)]
 
 
-def getImage(sheet, frame_x, frame_y, width, height, scale, color):
-    image = pygame.Surface((width, height)).convert_alpha()
+# def getImage(sheet, frame_x, frame_y, width, height, scale, color):
+#     image = pygame.Surface((width, height)).convert_alpha()
     
-    image.blit(sheet, (0, 0), ((frame_x * width), (64 * frame_y), width, (height * frame_x))) # area x = 0, y = 0, width, height
-    image = pygame.transform.scale(image, (width * scale, height * scale))
-    image.set_colorkey(color)
-    return image
+#     image.blit(sheet, (0, 0), ((frame_x * width), (64 * frame_y), width, (height * frame_x))) # area x = 0, y = 0, width, height
+#     image = pygame.transform.scale(image, (width * scale, height * scale))
+#     image.set_colorkey(color)
+#     return image
 
-BLACK = (0,0,0)
-frame_0 = getImage(player.player_sprite_idle, 0, 0, 64, 64, 2, BLACK)
+# frame_0 = getImage(player.player_sprite_idle, 0, 0, 64, 64, 2, BLACK)
 frame_x = 1
 frame_y = 0
 
@@ -45,21 +46,23 @@ while run:
    
 
     window.fill((100, 100, 100))
-    # player.render(window)
+    player.render(window)
     
-    window.blit(getImage(player.player_sprite_idle, frame_x, frame_y, 64, 64, 3, BLACK), (player.player_rect))
-    frame_x += 1
-    if frame_x == 6:
-        if frame_y <= 3:
-            frame_y += 1
-            if frame_y == 4:
-                frame_y = 0
+    # window.blit(getImage(player.player_sprite_idle, frame_x, frame_y, 64, 64, 3, BLACK), (player.player_rect))
+
+    # animation purpose
+
+    # frame_x += 1
+    # if frame_x == 6:
+    #     if frame_y <= 3:
+    #         frame_y += 1
+    #         if frame_y == 4:
+    #             frame_y = 0
         
-    if frame_x >= 6:
-        frame_x = 0
+    # if frame_x >= 6:
+    #     frame_x = 0
         
-    print(frame_x, frame_y)
-    # window.blit(player.player_sprite_idle, (0, 0))
+    # print(frame_x, frame_y)
     
     pygame.display.flip()
     clock.tick(FPS)
